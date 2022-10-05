a81a37bd62d76798e0ba43437ae59660bf4f2abd
Camilo2005 <105441872+ALKJSHD@users.noreply.github.com>
2022-10-05 16:47:48 -0500

    Add files via upload

---
+++ jcz.5.10/script/script.js
@@ -0,0 +1,38 @@
+let nombre=document.getElementById('firstName').value 
+let apellido=document.getElementById('lastName').value
+let formulario=document.getElementById('form').value
+let usuario=document.getElementById('username').valueen deseo Low con C. 
+let email=document.getElementById('email').value
+let adress=document.getElementById('adress').value
+let country=document.getElementById('country').value
+
+let formulario=document.getElementById('form').value
+
+formulario.addEventListener('submit',(e) =>{
+    e.preventDefault()
+
+let nombre,apellido,formulario,usuario,email,adress,country
+
+let formulario=document.getElementById('form')
+
+formulario.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    function guardarLocalStorage(
+})
+
+function guardarLocalStorage(){
+    localStorage.setItem('nombre', nombre)
+    localStorage.setItem('apellido', apellido)
+}
+function leerData(){
+let nombre=document.getElementById('firstName').value 
+ apellido=document.getElementById('lastName').value
+ formulario=document.getElementById('form').value
+ usuario=document.getElementById('username').value
+ email=document.getElementById('email').value
+ adress=document.getElementById('adress').value
+ country=document.getElementById('country').value
+
+
+})
+
